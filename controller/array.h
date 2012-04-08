b9752291ab88852b7ba80005349317c2abec2bf8
Michael Moon <triffid.hunter@gmail.com>
2012-04-08 14:58:24 +1000

    add warn_unused_result on array enlarging functions since realloc likes to relocate the whole array to a new location

--- controller/array.h
+++ controller/array.h
@@ -7,12 +7,17 @@ typedef struct {
 	void * data[];
 } array;
 
-array* array_init(void);
-array* array_push(array *a, void *element);
-array* array_unshift(array *a, void *element);
+array* array_init(void) __attribute__ ((__warn_unused_result__));
+void array_free(array *a);
+
+array* array_push(array *a, void *element) __attribute__ ((__warn_unused_result__));
 void* array_pop(array *a);
+
+array* array_unshift(array *a, void *element) __attribute__ ((__warn_unused_result__));
 void* array_shift(array *a);
+
 int array_indexof(array *a, void *element);
-array* array_delete(array *a, void *element);
+
+array* array_delete(array *a, void *element) __attribute__ ((__warn_unused_result__));
 
 #endif	/* _ARRAY_H */
